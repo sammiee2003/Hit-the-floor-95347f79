1432f18f60e370b518e448eaa6a3892c11858667
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-03-25 14:52:28 +0100

    lvl 4 opdr 2

--- wisselgeld.php
+++ wisselgeld.php
@@ -3,7 +3,7 @@ $input = readline(" ");
 $geld = array(10,5,2,1);
 
 foreach ($geld as $value) {
-    if ($input >= 1){
+    if ($input >= 1) {
         $rest = floor($input / $value);
         $input = $input - ($value * $rest);
         echo $rest . "x" . $value . "euro" . PHP_EOL;
